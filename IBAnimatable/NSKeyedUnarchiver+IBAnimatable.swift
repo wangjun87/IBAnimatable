332befea68736183a7dfa35d248030762f445107
Jake Lin <jakelinau@gmail.com>
2016-01-22 23:54:23 +1100

    WIP: Remove unused code.

--- IBAnimatable/NSKeyedUnarchiver+IBAnimatable.swift
+++ IBAnimatable/NSKeyedUnarchiver+IBAnimatable.swift
@@ -43,39 +43,3 @@ public extension NSKeyedUnarchiver {
     }
   }
 }
-
-extension UIViewController {
-  public override class func initialize() {
-    struct Static {
-      static var token: dispatch_once_t = 0
-    }
-    
-    // make sure this isn't a subclass
-    if self !== UIViewController.self {
-      return
-    }
-    
-    dispatch_once(&Static.token) {
-      let originalSelector = Selector("viewWillAppear:")
-      let swizzledSelector = Selector("nsh_viewWillAppear:")
-      
-      let originalMethod = class_getInstanceMethod(self, originalSelector)
-      let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
-      
-      let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
-      
-      if didAddMethod {
-        class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
-      } else {
-        method_exchangeImplementations(originalMethod, swizzledMethod)
-      }
-    }
-  }
-  
-  // MARK: - Method Swizzling
-  
-  func nsh_viewWillAppear(animated: Bool) {
-    self.nsh_viewWillAppear(animated)
-    print("viewWillAppear: \(self)")
-  }
-}
